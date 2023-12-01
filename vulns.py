b952ce2aed92d652a5242c80b9b32c0906e4ab80
SidneyJob <SidneyJob13@gmail.com>
2023-12-01 22:27:41 +0300

    init new

--- vulns.py
+++ vulns.py
@@ -57,50 +57,6 @@ def reset_password():
 
 
 try:
-    @app.route('/api_without_creds', methods=['POST', 'GET'])
-    def api_creds():
-        res = Response(json.dumps(
-            {"key":None}
-        ))
-        
-        res.headers['Content-Type'] = "application/json"
-        res.headers['Access-Control-Allow-Origin'] = "*"    
-
-        cookies = []
-        if request.cookies.get('None'):
-            cookies.append(f'None: {request.cookies.get("Strict")}')
-
-        if request.cookies.get('Lax'):
-            cookies.append(f'Lax: {request.cookies.get("Lax")}')
-
-        if request.cookies.get('Strict'):
-            cookies.append(f'Strict: {request.cookies.get("Strict")}')
-
-        if cookies:
-            print_c(f"""
- <----------------------------------------->
-            !!!!!! WARNING !!!!!!
-[+] Authentification passed!
-[+] URL: {request.url}
-[*] Recived cookies: [
-{print_cookies(cookies)}
-]
- <----------------------------------------->
-            """,
-            'RED'
-        )
-            
-            res.data = json.dumps(
-                {"key":f"SidneyJob{{{gen_random_string(16)}_creds_page}}"}
-            )
-
-            return res
-        return res
-
-
-
-
-
     @app.route('/api_gen', methods=['POST', 'GET'])
     def api_gen():
         res = Response(json.dumps(
@@ -139,6 +95,50 @@ try:
             
             res.data = json.dumps(
                 {"key":f"SidneyJob{{{gen_random_string(16)}_gen_page}}"}
+            )
+
+            return res
+        return res
+
+
+
+
+
+    @app.route('/api_without_creds', methods=['POST', 'GET'])
+    def api_creds():
+        res = Response(json.dumps(
+            {"key":None}
+        ))
+        
+        res.headers['Content-Type'] = "application/json"
+        res.headers['Access-Control-Allow-Origin'] = "*"    
+
+        cookies = []
+        if request.cookies.get('None'):
+            cookies.append(f'None: {request.cookies.get("Strict")}')
+
+        if request.cookies.get('Lax'):
+            cookies.append(f'Lax: {request.cookies.get("Lax")}')
+
+        if request.cookies.get('Strict'):
+            cookies.append(f'Strict: {request.cookies.get("Strict")}')
+
+        if cookies:
+            print_c(f"""
+ <----------------------------------------->
+            !!!!!! WARNING !!!!!!
+[+] Authentification passed!
+[+] URL: {request.url}
+[*] Recived cookies: [
+{print_cookies(cookies)}
+]
+ <----------------------------------------->
+            """,
+            'RED'
+        )
+            
+            res.data = json.dumps(
+                {"key":f"SidneyJob{{{gen_random_string(16)}_creds_page}}"}
             )
 
             return res
