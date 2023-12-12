8917ec19a4fbed946988c054d6e74cc128761e91
SidneyJob <SidneyJob13@gmail.com>
2023-12-12 20:55:26 +0300

    Fixed new_func

--- new_func.py
+++ new_func.py
@@ -22,8 +22,7 @@ def reset_password():
     return "123"
 
 def check_result(request):
-    cookies = []
-    
+    authorized = 0
 
     if request.method == "GET":
         data = dict(request.args)
@@ -37,24 +36,28 @@ def check_result(request):
 
 
     ############ PRINT DATA
-    print_c(f"""{"+"*100}\n{(100//2 - len("REQUEST INFO")) * "+"}REQUEST INFO{(100//2)  * "+"}\n{"+"*100}""", "RED")
-    print_c(f"""
-[+] URL: {request.url}
-[+] METHOD: {request.method}
-""", "RED")
+    print_c(f"""{(100//2 - len("REQUEST INFO")) * "+"}REQUEST INFO{(100//2)  * "+"}""", "RED")
+    print_c(f"""[+] URL: {request.url}\n[+] Method: {request.method}\n""", "RED")
 
-    print_c(f"[++] Cookies: \n{'+' * 100 }", "GREEN")
+    print_c(f"[++] Cookies:", "GREEN")
     for cookie_name, cookie_value in dict(request.cookies).items():
-        print_c(f"[+] {cookie_name}: {cookie_value}", "MAGENTA")
+        print_c(f"    [+] {cookie_name}: {cookie_value}", "MAGENTA")
+        if cookie_name:
+            authorized = 1
 
-    print_c(f"{'+' * 100}\n", "GREEN")
-
-
-
-    print_c(f"[++] Data: \n{'+'*100}", "BLUE")
+    print_c(f"\n[++] Data:", "BLUE")
     for data_name, data_value in data.items():
-        print_c(f"[+] {data_name}: {data_value}","MAGENTA")    
-    print_c(f"{'+' * 100}", "BLUE")
+        print_c(f"    [+] {data_name}: {data_value}","MAGENTA")    
 
-    print_c(f"""{"+"*100}\n{(100//2 - len("END")) * "+"}END{(100//2)  * "+"}\n{"+"*100}""", "RED")
+    print_c(f"\n[++] Headers:", "YELLOW")
+
+    for header_name, header_value in dict(request.headers).items():
+        print_c(f"    [+] {header_name}: {header_value}","MAGENTA")    
+    
+    print_c(f"""{(100//2 - len("END")) * "+"}END{(100//2)  * "+"}""", "RED")
+    if authorized:
+        return True
+    else:
+        return False
+
 app.run(host=app.host, port=app.port)
