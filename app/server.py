8cb4a6d37612ce0fd2e10147347472cab57200ad
tokiakasu <abulgakovvv@yandex.ru>
2024-01-04 15:47:49 +0300

    Add option to enable or disable usage of SSL certs

--- app/server.py
+++ app/server.py
@@ -1,19 +1,24 @@
 from flask import Flask
 from views import lab_views
-from logger import setup_logger
-# import ssl
+from logger import setup_logger, check_decision
+import ssl
 import os
-
-# ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-# ctx.load_cert_chain('certs/cert.pem', 'certs/privkey.pem')
 
 app = Flask(__name__, template_folder='templates')
 app.register_blueprint(lab_views, url_prefix="/", name="lab_views")
 
 app.secret_key = os.getenv("SECRET_KEY")
-app.debug = os.getenv("FLASK_DEBUG", 'False').lower() in ['1', 'true']
+app.debug = check_decision("FLASK_DEBUG", 'False')
 
 logger = setup_logger()
+
+flask_run_cert = check_decision("FLASK_RUN_CERT", 'False')
+if flask_run_cert:
+    ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ctx.load_cert_chain('certs/cert.pem', 'certs/privkey.pem')
+    logger.info("Server will use SSL certificates")
+else:
+    ctx = None
 
 # Start server
 if __name__ == "__main__":
@@ -21,6 +26,5 @@ if __name__ == "__main__":
     app.run(
         host='0.0.0.0',
         port=8081,
-        # debug=True,
-        # ssl_context=ctx
+        ssl_context=ctx
         )
